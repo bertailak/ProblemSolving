2c4cac4708d224266bf7d42734ba3d88420f6cec
bertailak <bertailak@gmail.com>
2022-03-09 08:59:42 +0600

    HasCycle

---
+++ leetcode/HasCycle.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import leetcode.model.ListNode;
+
+public class HasCycle {
+
+    // https://leetcode.com/problems/linked-list-cycle/
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        ListNode walker = head;
+        ListNode runner = head;
+        while (runner.next != null && runner.next.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner)
+                return true;
+        }
+        return false;
+    }
+
+}
